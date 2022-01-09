c103ec5b6506f55f9962b080067ae53ddde89122
annaindistress <annaindistress@gmail.com>
2022-01-09 22:54:41 +0300

    Fundamentals - Part 1. Sections 1-12

---
+++ 01-fundamentals-part-1/assignments.js
@@ -0,0 +1,19 @@
+// VALUES AND VARIABLES
+
+let country = 'Russia';
+let continent = 'Europe and Asia';
+let population = 144.1;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// DATA TYPES
+
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
