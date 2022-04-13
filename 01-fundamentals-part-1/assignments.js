2a66291927761f125dffae03b0580c3fa5c4a6a4
annaindistress <annaindistress@gmail.com>
2022-04-13 22:26:58 +0300

    JavaScript Fundamentals - Part 1. Section 1-12

---
+++ 01-fundamentals-part-1/assignments.js
@@ -0,0 +1,19 @@
+// Values and Variables
+
+let country = 'Russia';
+let continent = 'Eurasia';
+let population = 145;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
