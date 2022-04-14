31ce156c8955a5518fe4ab49ed4575dd3e94e93e
annaindistress <annaindistress@gmail.com>
2022-04-14 20:41:42 +0300

    JavaScript Fundamentals - Part 1. Section 22

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -199,6 +199,7 @@ console.log(n);
 
 // Truthy and Falsy Values
 
+/*
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('Jonas'));
@@ -220,3 +221,28 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED');
 }
+*/
+
+// Equality Operators: == vs ===
+
+const age = '18';
+
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number(prompt('Waht\'s your favourite number?'));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number');
+} else if (favourite === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if (favourite !== 23) console.log('Why not 23?');
