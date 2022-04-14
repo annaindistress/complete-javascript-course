38ae3f23d99a9b479269239fe51451829f62c884
annaindistress <annaindistress@gmail.com>
2022-04-14 21:56:08 +0300

    JavaScript Fundamentals - Part 1. Section 24

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -225,6 +225,7 @@ if (height) {
 
 // Equality Operators: == vs ===
 
+/*
 const age = '18';
 
 if (age === 18) console.log('You just became an adult :D (strict)');
@@ -246,3 +247,30 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log('Why not 23?');
+
+*/
+
+// Logical Operators
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive');
+} else {
+    console.log('Someone else should drive...');
+}
+
+const isTired = true; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive');
+} else {
+    console.log('Someone else should drive...');
+}
