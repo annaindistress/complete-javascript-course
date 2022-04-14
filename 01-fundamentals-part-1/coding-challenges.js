509110f84ee98c77cece5d26efe1da520da888c9
annaindistress <annaindistress@gmail.com>
2022-04-14 22:18:34 +0300

    JavaScript Fundamentals - Part 1. Coding Challenge #3

--- 01-fundamentals-part-1/coding-challenges.js
+++ 01-fundamentals-part-1/coding-challenges.js
@@ -30,3 +30,48 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+// Coding Challenge #3
+
+// Test Data 1
+
+// const firstGameDolphins = 96;
+// const secondGameDolphins = 108;
+// const thirdGameDolphins = 89;
+
+// const firstGameKoalas = 88;
+// const secondGameKoalas = 91;
+// const thirdGameKoalas = 110;
+
+// Test Data 2
+
+// const firstGameDolphins = 97;
+// const secondGameDolphins = 112;
+// const thirdGameDolphins = 101;
+
+// const firstGameKoalas = 109;
+// const secondGameKoalas = 95;
+// const thirdGameKoalas = 123;
+
+// Test Data 3
+
+const firstGameDolphins = 97;
+const secondGameDolphins = 112;
+const thirdGameDolphins = 101;
+
+const firstGameKoalas = 109;
+const secondGameKoalas = 95;
+const thirdGameKoalas = 106;
+
+const averageDolphins = (firstGameDolphins + secondGameDolphins +thirdGameDolphins) / 3;
+const averageKoalas = (firstGameKoalas + secondGameKoalas + thirdGameKoalas) / 3;
+
+if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (averageDolphins < averageKoalas && averageKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (averageDolphins === averageKoalas && averageDolphins >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy 😭');
+}
