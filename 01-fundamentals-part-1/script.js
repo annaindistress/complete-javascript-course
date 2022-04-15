4fbb59d81730f67343d20f390198f70b01845463
annaindistress <annaindistress@gmail.com>
2022-04-15 11:22:00 +0300

    JavaScript Fundamentals - Part 1. Section 26

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -252,6 +252,7 @@ if (favourite !== 23) console.log('Why not 23?');
 
 // Logical Operators
 
+/*
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
 
@@ -273,4 +274,47 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log('Sarah is able to drive');
 } else {
     console.log('Someone else should drive...');
+}
+*/
+
+// The switch Statement
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan my course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory video');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan my course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory video');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
 }
