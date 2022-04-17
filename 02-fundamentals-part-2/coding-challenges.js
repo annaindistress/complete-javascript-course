4f5d7cc96cf113158730ecb3e2e21a736cded511
annaindistress <annaindistress@gmail.com>
2022-04-17 21:32:25 +0300

    JavaScript Fundamentals - Part 2. Coding Challenge #2

--- 02-fundamentals-part-2/coding-challenges.js
+++ 02-fundamentals-part-2/coding-challenges.js
@@ -25,3 +25,27 @@ checkWinner(avgDolphins, avgKoalas);
 avgDolphins = calcAverage(85, 54, 41);
 avgKoalas = calcAverage(23, 34, 27);
 checkWinner(avgDolphins, avgKoalas);
+
+// Coding Challenge #2
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+
+console.log(bills);
+
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2]),
+];
+
+console.log(tips);
+
+const totals = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2],
+];
+
+console.log(totals);
