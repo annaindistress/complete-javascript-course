ac546d35819c21b71fe1be8e9c168037f43246d2
annaindistress <annartmva@gmail.com>
2022-06-09 10:32:31 +0300

    Data Structures, Modern Operators and Strings. Section 101 - 104

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -2,53 +2,58 @@
 
 // Data needed for a later exercise
 const flights =
-    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    fri: {
+      open: 11,
+      close: 23,
     },
-    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-        console.log(
-            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-        );
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
     },
+  },
+
+  order: function (startetIndex, mainIndex) {
+    return [this.starterMenu[startetIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
 };
 
 // Destructuring Arrays
 
 /*
 const arr = [2, 3, 4];
+
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
 const [x, y, z] = arr;
+
+console.log(a, b, c);
 console.log(x, y, z);
-console.log(arr);
 
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
+
+// Switching variables
 
 // const temp = main;
 // main = secondary;
@@ -58,53 +63,69 @@ console.log(main, secondary);
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-const [starter, mainCourse]  = restaurant.order(2, 0);
+const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
+// Nested destructoring
+
 const nested = [2, 4, [5, 6]];
+
 // const [i, , j] = nested;
-const [i,  , [j, k]] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
-const [p = 1, q = 1, m = 1] = [8, 9];
-console.log(p, q, m);
+// Default values
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
 */
 
 // Destructuring Objects
 
+/*
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
 const {
-    name: restaurantName,
-    openingHours: hours,
-    categories: tags,
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
 } = restaurant;
 console.log(restaurantName, hours, tags);
+
+// Default values
 
 const { menu = [], starterMenu: starters = [] } = restaurant;
 console.log(menu, starters);
 
+// Mutating variables
 let a = 111;
 let b = 999;
 const obj = { a: 23, b: 7, c: 14 };
 
 ({ a, b } = obj);
+
 console.log(a, b);
 
+// Nested objects
+
 const {
-    fri: { open: o, close: c },
+  fri: { open, close },
 } = openingHours;
-console.log(o, c);
+console.log(open, close);
 
 restaurant.orderDelivery({
-    time: '22:30',
-    address: 'Via del Sole, 21',
-    mainIndex: 2,
-    starterIndex: 2,
-});
-
-restaurant.orderDelivery({
-  address: 'Via del Sole, 21',
+  time: '22:30',
+  address: 'Via del Sol, 21',
+  mainIndex: 2,
   starterIndex: 2,
 });
+
+restaurant.orderDelivery({
+  address: 'Via del Sol, 21',
+  starterIndex: 1,
+});
+
+*/
