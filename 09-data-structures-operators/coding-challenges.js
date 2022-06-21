942c8d04ae10110fa637ee808cb3246c34139617
annaindistress <annartmva@gmail.com>
2022-06-21 21:21:45 +0300

    Data Structures, Modern Operators and Strings. Coding Challenge #2

--- 09-data-structures-operators/coding-challenges.js
+++ 09-data-structures-operators/coding-challenges.js
@@ -1,48 +1,49 @@
 'use strict';
 
-// Coding Challenge #1
-
 const game = {
-	team1: 'Bayern Munich',
-	team2: 'Borrussia Dortmund',
-	players: [
-		[
-			'Neuer',
-			'Pavard',
-			'Martinez',
-			'Alaba',
-			'Davies',
-			'Kimmich',
-			'Goretzka',
-			'Coman',
-			'Muller',
-			'Gnarby',
-			'Lewandowski',
-		],
-		[
-			'Burki',
-			'Schulz',
-			'Hummels',
-			'Akanji',
-			'Hakimi',
-			'Weigl',
-			'Witsel',
-			'Hazard',
-			'Brandt',
-			'Sancho',
-			'Gotze',
-		],
-	],
-	score: '4:0',
-	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-	date: 'Nov 9th, 2037',
-	odds: {
-		team1: 1.33,
-		x: 3.25,
-		team2: 6.5,
-	},
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
+// Coding Challenge #1
+
+/*
 // 1.
 const [players1, players2] = game.players;
 
@@ -56,14 +57,16 @@ const allPlayers = [...players1, ...players2];
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
 //5.
-const {odds: {team1, x: draw, team2}} = game;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
 
 // 6.
-const printGoals = function ( ...players ) {
-	console.log(`${players.length} goals were scored:`);
-	for (let i = 0; i < players.length; i++) {
-		console.log(`    - ${players[i]}`);
-	}
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored:`);
+  for (let i = 0; i < players.length; i++) {
+    console.log(`    - ${players[i]}`);
+  }
 };
 
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
@@ -72,3 +75,35 @@ printGoals(...game.scored);
 // 7.
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
+*/
+
+// Coding Challenge #2
+
+// 1.
+for (const [i, name] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${name}`);
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) average += odd;
+average /= odds.length;
+
+console.log(`Average odd: ${average}`);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+// 4.
+const scorers = {};
+
+for (const scorer of game.scored)
+  scorers[scorer] = Object.keys(scorers).includes(scorer)
+    ? ++scorers[scorer]
+    : 1;
+
+console.log(scorers);
