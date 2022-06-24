90d6764d674ce41971243f58eceb08586bb5a2a9
annaindistress <annartmva@gmail.com>
2022-06-24 16:42:46 +0300

    A Closer Look at Functions. Coding Challenge #2

--- 10-functions/coding-challenges.js
+++ 10-functions/coding-challenges.js
@@ -39,3 +39,14 @@ poll.displayResults.call({answers: [5, 2, 3]}, 'string');
 
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]});
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string');
+
+// Coding Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  })
+})();
