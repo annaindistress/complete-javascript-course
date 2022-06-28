d0338881e8c46300aa777212ab14d6e4f6893fb3
annaindistress <annartmva@gmail.com>
2022-06-28 11:27:28 +0300

    Working With Arrays. Coding Challenge #2

--- 11-arrays-bankist/coding-challenge.js
+++ 11-arrays-bankist/coding-challenge.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Coding Challenge #1
+
+/*
 const checkDogs = (dogsJulia, dogsKate) => {
   const dogsJuliaCorrected = dogsJulia.slice(1, -2);
   const dogs = dogsJuliaCorrected.concat(dogsKate);
@@ -17,3 +20,28 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 console.log('--- TEST DATA 2 ---');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+// Coding Challenge #2
+
+/*
+const calcAverageHumanAge = ages => {
+  // const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  // const adults = humanAges.filter(humanAge => humanAge >= 18);
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  // return average;
+
+  return ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, age, _, arr) => acc + age / arr.length, 0);
+};
+
+console.log('--- TEST DATA 1 ---');
+console.log([5, 2, 4, 1, 15, 8, 3]);
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+
+console.log('--- TEST DATA 2 ---');
+console.log([16, 6, 10, 5, 6, 1, 4]);
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
